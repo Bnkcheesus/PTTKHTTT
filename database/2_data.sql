5b77f63192f70d25e5d00138d63b424b2190d20a
Song Chi <111693600+Bnkcheesus@users.noreply.github.com>
2026-05-11 13:05:56 +0700

    cập nhật database

--- database/2_data.sql
+++ database/2_data.sql
@@ -1026,3 +1026,15 @@ INSERT INTO HOADON (MaHD, LoaiHoaDon, SoTien, NgayThanhToan, MaPhieuTra) VALUES 
 INSERT INTO HOADON (MaHD, LoaiHoaDon, SoTien, NgayThanhToan, MaPhieuTra) VALUES ('HDON048', N'Thanh toán đợt cuối', 800000, '2024-04-02', 'PTP048');
 INSERT INTO HOADON (MaHD, LoaiHoaDon, SoTien, NgayThanhToan, MaPhieuTra) VALUES ('HDON049', N'Thanh toán đợt cuối', 400000, '2024-06-15', 'PTP049');
 INSERT INTO HOADON (MaHD, LoaiHoaDon, SoTien, NgayThanhToan, MaPhieuTra) VALUES ('HDON050', N'Thanh toán đợt cuối', 700000, '2024-05-08', 'PTP050');
+
+
+-- Cập nhật trạng thái phòng thành 'Đã cho thuê' nếu tất cả giường đều đã có người
+UPDATE PHONG
+SET TrangThai = N'Đã cho thuê'
+WHERE MaPhong IN (
+    SELECT MaPhong
+    FROM GIUONG
+    GROUP BY MaPhong
+    HAVING COUNT(*) > 0 
+    AND COUNT(CASE WHEN TrangThai = N'Trống' THEN 1 END) = 0
+);
