82763de7821508c25633cedfcd9bd22e2e0f28e6
Thuy Kim <148269903+mikyuht@users.noreply.github.com>
2026-05-11 23:20:00 +0700

    Merge branch 'main' into thuy-traphong

--- database/5_sp_XacNhanThue.sql
+++ database/5_sp_XacNhanThue.sql
@@ -59,27 +59,73 @@ GO
 
 -- Thêm Phiếu Đặt Cọc
 CREATE OR ALTER PROCEDURE ThemPDC
-    @TrangThai NVARCHAR(50), --Da thanh toan, Chua thanh toan, ...
     @TienCoc DECIMAL(18, 2),
     @MaKH VARCHAR(50),
-    @MaNV VARCHAR(50),
+    @MaNV VARCHAR(50) = NULL, --Default to null
     @MaPhong VARCHAR(50),
-	@MaPhieuYC VARCHAR(50)
+    @MaPhieuYC VARCHAR(50)
 AS
 BEGIN
+    SET NOCOUNT ON;
+
     DECLARE @NextNum INT;
     DECLARE @MaPhieuDatCoc VARCHAR(50);
 
-    -- Tự động sinh mã PDC mới
-    SELECT @NextNum = ISNULL(MAX(CAST(SUBSTRING(MaPhieuDatCoc, 4, 10) AS INT)), 0) + 1 FROM PHIEUDATCOC;
+    DECLARE @HinhThucThue NVARCHAR(100);
+    DECLARE @SoNguoiDuKien INT;
+
+    -- Variables xử lý giường
+    DECLARE @MaGiuong VARCHAR(50);
+
+    --------------------------------------------------------
+    -- Lấy thông tin phiếu yêu cầu
+    --------------------------------------------------------
+    SELECT 
+        @HinhThucThue = HinhThucThue,
+        @SoNguoiDuKien = SoNguoiDuKien
+    FROM PHIEUYEUCAU
+    WHERE MaPhieuYC = @MaPhieuYC;
+
+    --------------------------------------------------------
+    -- Sinh mã PDC mới
+    --------------------------------------------------------
+    SELECT 
+        @NextNum = ISNULL(MAX(CAST(SUBSTRING(MaPhieuDatCoc, 4, 10) AS INT)), 0) + 1
+    FROM PHIEUDATCOC;
+
     SET @MaPhieuDatCoc = 'PDC' + RIGHT('000' + CAST(@NextNum AS VARCHAR(10)), 3);
 
-    -- Insert vào DB
-    INSERT INTO PHIEUDATCOC (MaPhieuDatCoc, NgayLap, LoaiDatCoc, TrangThai, TienCoc, MaKH, MaNV, MaPhong, MaPhieuYC)
-    VALUES (@MaPhieuDatCoc, GETDATE(), N'Cọc giữ chỗ', N'Chưa thanh toán', @TienCoc, @MaKH, @MaNV, @MaPhong, @MaPhieuYC);
+    --------------------------------------------------------
+    -- Insert PHIEUDATCOC
+    --------------------------------------------------------
+    INSERT INTO PHIEUDATCOC
+    (
+        MaPhieuDatCoc,
+        NgayLap,
+        LoaiDatCoc,
+        TrangThai,
+        TienCoc,
+        MaKH,
+        MaNV,
+        MaPhong,
+        MaPhieuYC
+    )
+    VALUES
+    (
+        @MaPhieuDatCoc,
+        GETDATE(),
+        N'Cọc giữ chỗ',
+        N'Chưa thanh toán',
+        @TienCoc,
+        @MaKH,
+        @MaNV,
+        @MaPhong,
+        @MaPhieuYC
+    );
 
-    -- Trả về mã vừa sinh ra
+    
     SELECT @MaPhieuDatCoc AS MaPhieuDatCocMoi;
+
 END
 GO
 
@@ -105,3 +151,20 @@ BEGIN
     VALUES (@MaPhieuDatCoc, @MaGiuong);
 END
 GO
+CREATE OR ALTER PROCEDURE CapNhatThongTinKH
+    @MaKH VARCHAR(50),
+    @HoTen NVARCHAR(100),
+    @SDT VARCHAR(20),
+    @Email VARCHAR(100),
+    @GioiTinh NVARCHAR(10)
+AS
+BEGIN
+    UPDATE KHACHHANG
+    SET HoTen = @HoTen, 
+        SDT = @SDT, 
+        Email = @Email, 
+        GioiTinh = @GioiTinh
+    WHERE MaKH = @MaKH
+END
+GO
+
